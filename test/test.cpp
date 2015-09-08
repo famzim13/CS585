25e12386f1fb69cfae4d1939f6d212da57e83363
Alan Zimmer <azimmer1992@gmail.com>
2015-09-08 17:47:40 -0400

    Update to making GTest work

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,20 @@
 // test.cpp
-#include "test.h"
+#include <gtest/gtest.h>
+
+TEST( AdditionTest, Equal )
+{
+    ASSERT_EQ( 5+5, 10 );
+}
+
+TEST( SubtractionTest, LessThan )
+{
+    ASSERT_LT( 10-9, 2 );
+}
+
+TEST( MultiplicationTest, GreaterThan )
+{
+    ASSERT_GT( 5*4, 15 );
+}
 
 int main(int argc, char **argv)
 {
