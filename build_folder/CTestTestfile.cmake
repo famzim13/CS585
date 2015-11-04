a0cb0dc827a692ef9af973dc80637fd066c5e430
Alan Zimmer <azimmer1992@gmail.com>
2015-11-03 19:03:46 -0500

    Removed build_folder files which passed ignore, updated ignore

--- build_folder/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alan/Desktop/Courses/CS 585/CS585
-# Build directory: /home/alan/Desktop/Courses/CS 585/CS585/build_folder
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(lib/gtest-1.7.0)
