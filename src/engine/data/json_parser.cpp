a42de148a8c9d9d5ba64f4c2b49f683caf6f5d60
Alan Zimmer <azimmer1992@gmail.com>
2015-12-05 20:07:54 -0500

    I wish I could go back up and beat up beginning semester me

--- src/engine/data/json_parser.cpp
+++ src/engine/data/json_parser.cpp
@@ -24,20 +24,52 @@ JsonEntity* JsonParser::fromString( const std::string& rawJson )
 {
     for( int i=0; i < rawJson.length(); i++ )
     {
-      std::cout << rawJson[i];
+      if( rawJson[i] == '[' )
+      {
+        i = parseArray( rawJson, i );
+      }
+      else if( rawJson[i] == 't' || rawJson[i] == 'f' )
+      {
+        i = parseBool( rawJson, i );
+      }
+      else if( rawJson[i] == '{' )
+      {
+        i = parseObject( rawJson, i );
+      }
+      else if( rawJson[i] == '-' || ( rawJson[i] >= '0' && rawJson[i] <= '9' ) )
+      {
+        i = parseNumber( rawJson, i );
+      }
+      else if( rawJson[i] == '"' )
+      {
+        i = parseString( rawJson, i );
+      }
     }
-    std::cout << '\n';
 }
 
 // MEMBER FUNCTIONS
 int JsonParser::parseArray( const std::string& rawJson, int pos )
 {
+    std::cout << "Parsing array\n";
+    while( rawJson[pos] != ']' )
+    {
+      std::cout << pos << " has value " << rawJson[pos] << "\n";
+      pos++;
+    }
 
+    return pos+1;
 }
 
 int JsonParser::parseBool( const std::string& rawJson, int pos )
 {
+    std::cout << "Parsing bool\n";
+    while( rawJson[pos] != 'e' )
+    {
+      std::cout << pos << " has value " << rawJson[pos] << "\n";
+      pos++;
+    }
 
+    return pos+1;
 }
 
 int JsonParser::parseNumber( const std::string& rawJson, int pos )
@@ -47,12 +79,26 @@ int JsonParser::parseNumber( const std::string& rawJson, int pos )
 
 int JsonParser::parseObject( const std::string& rawJson, int pos )
 {
+    std::cout << "Parsing object\n";
+    while( rawJson[pos] != '}' )
+    {
+      std::cout << pos << " has value " << rawJson[pos] << "\n";
+      pos++;
+    }
 
+    return pos+1;
 }
 
 int JsonParser::parseString( const std::string& rawJson, int pos )
 {
+    std::cout << "Parsing string\n";
+    while( rawJson[pos] != '"' )
+    {
+      std::cout << pos << " has value " << rawJson[pos] << "\n";
+      pos++;
+    }
 
+    return pos+1;
 }
 
 // Stuff to know
